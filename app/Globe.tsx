38d8707e7e7e8ff91e378be78f16975a95056bfc
Seth McCullough <seth@endevrs.dev>
2023-01-06 19:44:26 -0500

    Update cities array and globe props

--- app/Globe.tsx
+++ app/Globe.tsx
@@ -2,57 +2,44 @@
 
 import createGlobe, { type Marker } from "cobe";
 import { useEffect, useRef } from "react";
-import { useSpring } from "react-spring";
 
 import cities from "@/data/cities";
 
-export default function Globe() {
-  const markers = Object.entries(cities).map(([, { lat, lng }]) => ({
-    location: [lat, lng],
-    size: 0.1,
-  })) as Marker[];
+const markers = Object.entries(cities).map(([, { lat, lng }]) => ({
+  location: [lat, lng],
+  size: 0.1,
+})) as Marker[];
 
+export default function Globe() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const pointerInteracting = useRef<number | null>(null);
-  const pointerInteractionMovement = useRef(0);
-  const [{ r }, api] = useSpring(() => ({
-    r: 0,
-    config: {
-      mass: 1,
-      tension: 280,
-      friction: 40,
-      precision: 0.001,
-    },
-  }));
+
   useEffect(() => {
-    let width = 0;
-    const onResize = () =>
-      canvasRef.current && (width = canvasRef.current.offsetWidth);
-    window.addEventListener("resize", onResize);
-    onResize();
+    let phi = 0;
+
     const globe = createGlobe(canvasRef.current!, {
       devicePixelRatio: 2,
-      width: width * 2,
-      height: width * 2,
+      width: 1000,
+      height: 1000,
       phi: 0,
-      theta: 0.3,
+      theta: 0,
       dark: 1,
-      diffuse: 3,
+      diffuse: 1.2,
       mapSamples: 16000,
-      mapBrightness: 1.2,
-      baseColor: [1, 1, 1],
-      markerColor: [251 / 255, 100 / 255, 21 / 255],
-      glowColor: [1.2, 1.2, 1.2],
-      markers: [],
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers,
       onRender: (state) => {
-        state.phi = r.get();
-        state.width = width * 2;
-        state.height = width * 2;
+        state.phi = phi;
+        phi += 0.005;
       },
     });
-    setTimeout(() => (canvasRef.current!.style.opacity = "1"));
-    return () => globe.destroy();
-  }, [markers, r]);
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
   return (
     <div
       style={{
@@ -65,45 +52,7 @@ export default function Globe() {
     >
       <canvas
         ref={canvasRef}
-        onPointerDown={(e) => {
-          pointerInteracting.current =
-            e.clientX - pointerInteractionMovement.current;
-          canvasRef.current!.style.cursor = "grabbing";
-        }}
-        onPointerUp={() => {
-          pointerInteracting.current = null;
-          canvasRef.current!.style.cursor = "grab";
-        }}
-        onPointerOut={() => {
-          pointerInteracting.current = null;
-          canvasRef.current!.style.cursor = "grab";
-        }}
-        onMouseMove={(e) => {
-          if (pointerInteracting.current !== null) {
-            const delta = e.clientX - pointerInteracting.current;
-            pointerInteractionMovement.current = delta;
-            api.start({
-              r: delta / 200,
-            });
-          }
-        }}
-        onTouchMove={(e) => {
-          if (pointerInteracting.current !== null && e.touches[0]) {
-            const delta = e.touches[0].clientX - pointerInteracting.current;
-            pointerInteractionMovement.current = delta;
-            api.start({
-              r: delta / 100,
-            });
-          }
-        }}
-        style={{
-          width: "100%",
-          height: "100%",
-          cursor: "grab",
-          contain: "layout paint size",
-          opacity: 0,
-          transition: "opacity 1s ease",
-        }}
+        style={{ width: 600, height: 600, maxWidth: "100%", aspectRatio: 1 }}
       />
     </div>
   );
