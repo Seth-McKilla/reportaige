31ecab06c10dc116133ea8ecd22ec8226b516f5c
Seth McCullough <seth@endevrs.dev>
2023-01-06 20:46:09 -0500

    Update title placement

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import cities from "@/data/cities";
 export default async function Home() {
   return (
     <main>
-      <div className="my-10 text-center">
-        <h1 className="mb-2 text-4xl font-bold">reportAIge</h1>
+      <div className="mt-10 mb-20 text-center">
+        <h1 className="mb-4 text-4xl font-bold">reportAIge</h1>
         <p>
           AI generated reportage art based on current Twitter trends from around
           the world.
