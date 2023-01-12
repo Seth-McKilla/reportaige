9a912feef7daa992efc723cc15d9e5c7d38b92b5
Seth McCullough <seth@endevrs.dev>
2023-01-12 11:26:27 -0500

    Add disclaimer to ensure no flagged categories.

--- lib/openai.ts
+++ lib/openai.ts
@@ -12,14 +12,18 @@ export async function createArtworkDescription(
   hashtags: Artwork["hashtags"]
 ): Promise<string> {
   const max_tokens = 50;
-  const request = `Create a single sentence story in ${max_tokens} characters or less, without profanity, using five randomly chosen words or phrases from the following list: `;
+  const request = `Create a single, complete sentence story in ${max_tokens} characters or less, using five randomly chosen words or phrases from the following list: `;
 
   const inputWords = hashtags.reduce((acc, hashtag) => {
     const word = toLowerSpaceCase(hashtag);
     return `${acc}, ${word}`;
   }, "");
 
-  const prompt = request + inputWords;
+  // Moderation categories according to: https://beta.openai.com/docs/api-reference/moderations
+  const disclaimer =
+    ". The story should not include any of the following categories: hate, hate/threatening, self-harm, sexual, sexual/minors, violence, violence/graphic, or similar.";
+
+  const prompt = request + inputWords + disclaimer;
 
   try {
     const response = await openai.createCompletion({
