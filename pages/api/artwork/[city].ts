92ec76bc625055b7fdd28e8dbd5ac3ad3a8d63cb
Seth-McKilla <seth@endevrs.dev>
2023-01-14 12:39:35 -0500

    Check auth equality

--- pages/api/artwork/[city].ts
+++ pages/api/artwork/[city].ts
@@ -26,6 +26,10 @@ export default async function handler(
   if (authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
     console.log(req.headers.authorization);
     console.log(process.env.API_SECRET_KEY);
+    console.log(
+      "EQUALITY",
+      req.headers.authorization === process.env.API_SECRET_KEY
+    );
     return res.status(401).json({ error: "Unauthorized" });
   }
 
