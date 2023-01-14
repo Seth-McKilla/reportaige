cee4c59d3898995af53dbcf3c5696717590ca373
Seth-McKilla <seth@endevrs.dev>
2023-01-14 12:36:16 -0500

    Further logs for 401 prod debug

--- pages/api/artwork/[city].ts
+++ pages/api/artwork/[city].ts
@@ -24,7 +24,8 @@ export default async function handler(
   const { authorization } = req.headers;
 
   if (authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
-    console.log(req.headers);
+    console.log(req.headers.authorization);
+    console.log(process.env.API_SECRET_KEY);
     return res.status(401).json({ error: "Unauthorized" });
   }
 
